2b3d5a892cd49d7f10169e4851c7928441bf8064
Dhiraj <raj.pallin@gmail.com>
2014-10-12 11:20:54 +1000

    Removed dependant lib source, tools and screenshots

--- Mail Library/libetpan-1.5/build-windows/build_headers.bat
+++
@@ -1,5 +0,0 @@
-@mkdir include
-@mkdir include\libetpan
-@for /R ..\src\ %%x in (*.h) do @copy "%%x" include\libetpan
-@for %%x in (*.h) do @copy "%%x" include\libetpan
-@echo "done" >_headers_depends
--- Mail Library/libetpan-1.5/travis/before-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-sudo apt-get install libsasl2-dev libssl-dev zlib1g-dev
